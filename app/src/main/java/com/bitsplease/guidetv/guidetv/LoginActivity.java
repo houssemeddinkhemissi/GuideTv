3a49b7fedd1146f58ebc8469fc757cc72075526a
Houssem <HoussemEddinKhemissi@git.com>
2017-12-27 15:01:41 +0100

    AA

--- app/src/main/java/com/bitsplease/guidetv/guidetv/LoginActivity.java
+++ app/src/main/java/com/bitsplease/guidetv/guidetv/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity {
-    Button b1 , b2;
+
     FragmentManager f;
     FragmentTransaction tf;
     Fragment login,register;
@@ -19,41 +19,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-            b1 = (Button)findViewById(R.id.lb);
-            b2 = (Button)findViewById(R.id.rb);
+
          login = new LoginFragment();
          register = new RegisterFragment();
          f = getFragmentManager();
          tf = f.beginTransaction();
         tf.add(R.id.gu1,login);
         tf.commit();
-        b1.setPressed(true);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            b2.setPressed(false);
-            b1.setPressed(true);
-                f = getFragmentManager();
-                tf = f.beginTransaction();
-                tf.replace(R.id.gu1,login);
-                tf.commit();
+      
 
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                f = getFragmentManager();
-                tf = f.beginTransaction();
-                b1.setPressed(false);
-                b2.setPressed(true);
-
-                tf.replace(R.id.gu1,register);
-                tf.commit();
-
-
-            }
-        });
     }
 }
